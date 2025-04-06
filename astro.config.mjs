156723423faf938049c4490247dd1f718fe7b130
barrysir <14061419+barrysir@users.noreply.github.com>
2025-04-05 17:15:32 -0700

    Add Glitches and Tips pages, and plugin for custom header ids

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import {remarkHeadingId} from 'remark-custom-heading-id';
 
 // https://astro.build/config
 export default defineConfig({
+    markdown: {
+        remarkPlugins: [remarkHeadingId],
+    },
     integrations: [mdx()],
 });
